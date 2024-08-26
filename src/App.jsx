9ea42c22af085a2560c1672a8d9103d398f11422
sjl4365 <113270140+sjl4365@users.noreply.github.com>
2024-08-26 10:03:46 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import './App.css'
+import Home from './Home'
+
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Navbar from './Navbar';
+import Hackathon from './Hackathon';
+import Project from './Project';
+import Workexperience from './Workexperience';
+
+
+
+
+function App() {
+
+  return (
+    
+
+    // <Router basename='/MyWebsite'>
+
+    <Router basename='/MyWebsite'>
+      <div className='Route'>
+        <Navbar/>
+        
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/work" element={<Workexperience/>}/>
+
+          <Route path="/hackathon" element={<Hackathon/>}/>
+          <Route path="/project" element={<Project/>}/>
+
+
+      </Routes>
+      </div>
+    </Router>
+  );
+}
+export default App 
